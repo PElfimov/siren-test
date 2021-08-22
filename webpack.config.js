a682c36e73c179fcc0b6b6e34423dcbb793bf62a
Pavel Elfimov <pelfimov@yandex.ru>
2021-08-22 09:41:44 +0700

    rem extra file

--- webpack.config.js
+++ webpack.config.js
@@ -99,10 +99,6 @@ const config = {
           from: "./src/img",
           to: "./img",
         },
-        {
-          from: "./src/uploads",
-          to: "./uploads",
-        },
       ],
     }),
   ].concat(htmlPlugins),
